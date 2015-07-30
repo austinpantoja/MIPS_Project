a707f81ce421a2a11d566094fe671292c55cd753
Samantha Fahed <sam@samantha-faheds-computer.local>
2015-07-30 11:08:37 -0400

    edited comments and spacing

--- src/project.c
+++ src/project.c
@@ -5,7 +5,8 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-    switch(ALUControl) {
+    switch(ALUControl)
+    {
         case 0: // add
             *ALUresult = A + B;
             break;
@@ -31,28 +32,25 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
             *ALUresult = ~A;
             break;
     }
+
     *Zero = (*ALUresult == 0) ? 1 : 0;
 }
-
-
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {
-    //fetch the instruction addressed by PC from Mem and write it to instruction
-    //if PC is a multiple of 4 and is within memory
+    // fetch the instruction addressed by PC from Mem and write it to instruction
+    // if PC is a multiple of 4 and is within memory
     if(PC % 4 == 0 && PC <= 0xFFFF)
     {
         *instruction = Mem[PC >> 2];
         return 0;
     }
-    //halt condition occured - counter was not a multiple of 4 or was out of range
-    else 
+
+    else // Halt condition occured - counter was not a multiple of 4 or was out of range
         return 1;
 }
-
-
 
 /* instruction partition */
 /* 10 Points */
@@ -68,14 +66,12 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
     *jsec = instruction & 0x3ffffff;
 }
 
-
-
 /* instruction decode */
 /* 15 Points */
-//
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-    switch(op) {
+    switch(op)
+    {
         case 0: // r-types add, sub, and, or, slt, sltu
             controls->RegDst = 1;
             controls->Jump = 0;
@@ -180,18 +176,13 @@ int instruction_decode(unsigned op,struct_controls *controls)
     }
 }
 
-
-
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-    //assign value of register r1 to data1 and r2 to data2
-    *data1 = Reg[r1];
-    *data2 = Reg[r2];
+    *data1 = Reg[r1]; // Assign value of register r1 to data1
+    *data2 = Reg[r2]; // Assign value of register r2 to data2
 }
-
-
 
 /* Sign Extend */
 /* 10 Points */
@@ -200,22 +191,24 @@ void sign_extend(unsigned offset,unsigned *extended_value)
     //if negative, first 16 bits will be made 1s
     if ((offset >> 15) == 1)
         *extended_value = 0xFFFF0000 | offset;
+
     //if postive, first 16 bits will be made 0s
-    else 
+    else
         *extended_value = 0x0000FFFF & offset;
 }
-
-
 
 /* ALU operations */
 /* 10 Points */
 int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
 {
-    if (ALUSrc == 1) 
+    if (ALUSrc == 1)
         data2 = extended_value;
-    if (ALUOp == 7) {
+
+    if (ALUOp == 7)
+    {
         // add, sub, and, or, slt, sltu, left shitf
-        switch(funct) {
+        switch(funct)
+        {
             case 0: // sll (not sure if this is necessary for the 16 bit left shift)
                 ALUOp = 6;
                 break;
@@ -244,26 +237,26 @@ int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigne
                 return 1;
         }
     }
+
     ALU(data1, data2, ALUOp, ALUresult, Zero);
     return 0;
 }
-
-
 
 /* Read / Write Memory */
 /* 10 Points */
 int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
 {
-    //read the content of the memory location addressed by ALUresult to memdata
+    // Read the content of the memory location addressed by ALUresult to memdata
     if(MemRead == 1)
     {
-        //check for word alignment
+        // check for word alignment
         if(ALUresult % 4 != 0)
             return 1;
         else
             *memdata = Mem[ALUresult >> 2];
     }
-    //write the value of data2 to the memory location addressed by ALUresult
+
+    // write the value of data2 to the memory location addressed by ALUresult
     if(MemWrite == 1)
     {
         //check for word alignment
@@ -272,48 +265,49 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
         else
             Mem[ALUresult >> 2] = data2;
     }
+
     return 0;
 }
-
-
 
 /* Write Register */
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
-    if (RegWrite == 1) {
-        switch(MemtoReg) {
+    if (RegWrite == 1)
+    {
+        switch(MemtoReg)
+        {
             case 0:
                //Value from ALUresult
-               if (RegDst == 0) 
+               if (RegDst == 0)
                     Reg[r2] = ALUresult;
-               else 
+               else
                     Reg[r3] = ALUresult;
                break;
 
             case 1:
                //Value from memdata
-                if (RegDst == 0) 
+                if (RegDst == 0)
                     Reg[r2] = memdata;
-               else 
+               else
                     Reg[r3] = memdata;
                break;
         }
     }
 }
 
-
-
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
-    //increament by 4 everytime
+    // increament by 4 everytime
     *PC += 4;
-    //branch
+
+    // branch
     if(Branch == 1 && Zero == 1)
         *PC += (extended_value << 2);
-    //jump
+
+    // jump
     if(Jump == 1)
         *PC = (*PC & 0xF0000000) | ((jsec << 2) & 0x0FFFFFFF);
 }
